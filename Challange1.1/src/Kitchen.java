f75bf2043c75d428582a4749f3fb2da2d0d4a789
antony oduor <antonyoduor72@gmail.com>
2024-01-29 15:49:55 +0300

    Created 3 subclass; Refrigirator, DishWasher, CoffeeMaker

--- Challange1.1/src/Kitchen.java
+++ Challange1.1/src/Kitchen.java
@@ -1,2 +1,44 @@
 public class Kitchen {
+
+}
+class Refrigirator extends Kitchen{
+    protected boolean hasWorkToDo;
+
+    public Refrigirator(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood(){
+        System.out.println(">>Ordering Food..");
+    }
+}
+class DishWasher extends Kitchen{
+
+    protected boolean hasWorkToDo;
+
+    public DishWasher(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes(){
+
+        if(hasWorkToDo == true){
+        System.out.println(">>Cleaning Dishes..");
+    }else{
+            System.out.println(">>No Dishes!");
+        }
+}}
+class CoffeeMaker extends Kitchen{
+    protected boolean hasWorkToDo;
+
+    public CoffeeMaker(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    public void brewCoffee(){
+        if(hasWorkToDo == true){
+            System.out.println(">>Brewing coffee! ");
+        }else {
+            System.out.println(">>Empty");
+        }
+    }
 }
