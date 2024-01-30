2f5c1cf92e62c508fb6c88428a21a8375915a20a
antony oduor <antonyoduor72@gmail.com>
2024-01-30 08:10:29 +0300

    Created a constructor

--- Challange1.3/src/Printer.java
+++ Challange1.3/src/Printer.java
@@ -3,5 +3,15 @@ public class Printer {
     private int pagesPrinted;
     private boolean duplex;
 
-    
+    public Printer(int tonerLevel, int pagesPrinted, boolean duplex) {
+        if(tonerLevel > 100){
+            this.tonerLevel = 100;
+        }else if (tonerLevel <= 0){
+            this.tonerLevel = -1;
+        }else {
+            this.tonerLevel = tonerLevel;
+        }
+        this.pagesPrinted = pagesPrinted;
+        this.duplex = duplex;
+    }
 }
